d0317445bdcee269259433b8a24e2f0c55b83e93
Valeria <valeriakirkbride@gmail.com>
2023-08-16 19:41:24 -0600

    cleaned up

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function myFunction(imgs) {
+function changeImg(imgs) {
   var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "block";
 }
